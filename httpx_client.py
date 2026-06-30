80327ccd87c705ccf252655865dbce61fdb4202c
Владимир <vladimirlovinsky@gmail.com>
2026-06-30 12:38:59 +0300

    Добавлен модуль клиента httpx_client

--- httpx_client.py
+++ httpx_client.py
@@ -1,10 +1,22 @@
 import httpx
 
-# инициализируем клиент
-client = httpx.Client()
+# Проходим аутентификацию
+login_payload = {
+    "email": "user@example.com",
+    "password": "string"
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print('Login data:', login_response_data)
 
-# выполняем GET запрос, используя клиент
-response = client.get("http://localhost:8000/api/v1/users/me")
+# инициализируем клиент с base url
+client = httpx.Client(
+    base_url="http://localhost:8000",
+    timeout=100,
+    headers={"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+)
 
-# выводим ответ в консоль
-print(response.text)
+# Выполняем запрос с авторизацией
+get_user_me_response = client.get("/api/v1/users/me")
+get_user_me_response_data = get_user_me_response.json()
+print('Get user me data:', get_user_me_response_data)
